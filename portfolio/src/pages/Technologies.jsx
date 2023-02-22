6383d03eaeb8505b2323160bdac99bc056f333cd
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-22 16:34:04 +0100

    technologies skeleton

--- portfolio/src/pages/Technologies.jsx
+++ portfolio/src/pages/Technologies.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Technologies = () => {
     return (
-        <div>
-        Technologies.  
-        </div>
+        <>
+        <h2>Technologies</h2>
+        <section className="technologies">
+        <h3>
+        This portfolio was created using Vite, React et Sass.</h3>  
+        <div className="techs">HTML, CSS, REACT, VITE, PHP...</div>
+        </section>
+        </>
     );
 };
 
