9a39e678d6b60c43fc9ca1d45dbb1ae8198dcc5b
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-22 16:33:44 +0100

    journey page skeleton

--- portfolio/src/pages/Journey.jsx
+++ portfolio/src/pages/Journey.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import JourneyBlock from './components/JourneyBlock';
 
 const Journey = () => {
     return (
-        <div>
-            Journey  
-        </div>
+        <>
+            <h2>My journey</h2>
+            <JourneyBlock date="2023" title="Chapter II">Youhouhou</JourneyBlock>
+            <JourneyBlock date="November 2023" title="Chapter I">Lorem ipsum dolor sit amet consectetur <b>adipisicing elit</b>. Assumenda necessitatibus dicta repellendus omnis, quae velit consectetur atque voluptas accusantium nam, laborum cupiditate suscipit. Eveniet sed tempore nemo, itaque voluptatem sapiente impedit mollitia quasi earum labore. Ab, voluptate! Sequi corporis, similique veniam ipsam minus recusandae aspernatur kldjsfklj
+            dsfjkljdsf
+            </JourneyBlock>
+            <JourneyBlock date="Before 2023" title="Prologue">hkjhkjh</JourneyBlock>
+
+        </>
     );
 };
 
