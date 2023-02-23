b08eec63684d4646db84af98d2716933d4487b6e
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-23 14:04:24 +0100

    images?

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
     ]
   },
   {
-    path: "*",
+    path: '',
     element: <Error404/>,
     errorElement:<Error404/>,
     },
--- portfolio/src/pages/Me.jsx
+++ portfolio/src/pages/Me.jsx
@@ -10,9 +10,9 @@ const Me = () => {
         <h1><span><l>N</l><l>a</l><l>t</l><l>h</l><l>a</l><l>l</l><l>i</l><l>e</l></span> <span>Vanden Abeele</span></h1>
         <p>I am a Junior Full Stack Developer.</p>
         <p>This is my portfolio</p>
-        <Link to={'./journey'} className="link--accent" href="./pages/journey.html">discover my journey</Link>
+        <Link to={'./journey'} className="link--accent">discover my journey</Link>
         <div className="main__image bubble_two" id="hero_bubble">
-        <img id="hero_img" src="../src/assets/images/boat.png" alt="a picture of me"/></div>
+        <img id="hero_img" src={"/src/assets/images/boat.png"} alt="a picture of me"/></div>
         <div className="bubble_one"></div>
         </main>
         </>
